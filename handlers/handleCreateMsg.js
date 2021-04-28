035a24037139a22636d30080a0d9747662f5667e
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-04-28 15:51:42 +0530

    add !carbon & !term command

--- handlers/handleCreateMsg.js
+++ handlers/handleCreateMsg.js
@@ -1,13 +1,26 @@
-const { MessageMedia } = require('whatsapp-web.js');
+const {exec} = require('child_process');
 const short =  require("../modules/short");
+const genCarbon = require("../modules/Carbon");
 
-const handleCreateMsg = async (msg , client) => {
+const handleCreateMsg = async (msg , client , MessageMedia) => {
     if(msg.fromMe) {
-        if(msg.body.startsWith("!short")){
+        if(msg.body.startsWith("!short ")){
             msg.delete(true);
             short(msg.body.split('!short ')[1]).then(url => {
             client.sendMessage(msg.to, `${url.startsWith("https://") ? `Here is the shorten URL ${url}` : 'PLease send a valid url to short.'}`);
             })
+        }else if(msg.body.startsWith("!carbon ")){
+            msg.delete(true);
+            genCarbon(msg.body.split('!carbon ')[1]).then(data => {
+                const carbon = new MessageMedia(data.mimetype , data.data);
+                client.sendMessage(msg.to , carbon);
+            })
+        }else if(msg.body.startsWith('!term ')){
+           msg.delete(true);
+            exec(msg.body.split('!term ')[1] , (data , error) => {
+                console.log(error);
+                client.sendMessage(msg.to , data ? data : error);
+            });
         }
     }
 }
