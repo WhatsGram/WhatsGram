e8c1770752343f61820d3772a96934c4c5e4056b
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-05-17 12:25:50 +0530

    Add remove.bg API

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ const fs = require('fs');
 // const SESSION_DATA = process.env.SESSION_DATA || fs.existsSync('session.json') ? fs.readFileSync(__dirname + '/session.json', { encoding: 'utf8' }) : '';
 const TG_BOT_TOKEN = process.env.TG_BOT_TOKEN || '';
 const TG_OWNER_ID = process.env.TG_OWNER_ID || '';
+const REMOVE_BG_API = process.env.REMOVE_BG_API || undefined;
 
-module.exports = {TG_BOT_TOKEN , TG_OWNER_ID}
+module.exports = {TG_BOT_TOKEN , TG_OWNER_ID , REMOVE_BG_API}
