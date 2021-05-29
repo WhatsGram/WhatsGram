ec7d9edd80788e956a6c4ab6b115f3a567826aa5
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-05-29 12:54:39 +0530

    Fixed some errors & add a feature (see desc)
    Fixed error in sending file to tg and added feature to reply any file from tg

--- handlers/handleMessage.js
+++ handlers/handleMessage.js
@@ -3,6 +3,15 @@ const fs = require('fs');
 var path = require('path');
 
 const handleMessage = async (message , TG_OWNER_ID , tgbot) => {
+    const getMediaInfo = (msg) => {
+        switch (msg.type) {
+            case 'image': return {fileName: 'image.png' , tgFunc: tgbot.telegram.sendPhoto.bind(tgbot.telegram)}; break;
+            case 'video': return {fileName: 'video.mp4' , tgFunc: tgbot.telegram.sendVideo.bind(tgbot.telegram)}; break;
+            case 'audio': return {fileName: 'audio.m4a' , tgFunc: tgbot.telegram.sendAudio.bind(tgbot.telegram)}; break;
+            default: return {fileName: msg.body , tgFunc: tgbot.telegram.sendDocument.bind(tgbot.telegram)}; break;
+        }
+    }
+
     const chat = await message.getChat();
     const contact = await message.getContact(); 
     let name = contact.name || contact.pushname;
@@ -11,19 +20,17 @@ const handleMessage = async (message , TG_OWNER_ID , tgbot) => {
     }. \nSender Id : _${message.from.split("@")[0]}_ \nMessage Id: _${ message.id.id }_ \n\n${message.body ? `Message Body: \n👇👇👇👇\n${message.body}` : ""}`;
 
     if (message.hasMedia && !chat.isMuted) {
+        console.log(message)
         await message.downloadMedia().then(async (data) => {
-        let fileName;
-        message.type === "image" ? fileName = "document.png" : fileName =  message.body;
+        const mediaInfo = await getMediaInfo(message);
         const messageData = {
-            document : {source: path.join(__dirname , '../' , fileName)},
+            document : {source: path.join(__dirname , '../' , mediaInfo.fileName)},
             options: { caption: tgMessage, disable_web_page_preview: true, parse_mode: "markdown" }
         }
-        fs.writeFile(fileName , data.data, "base64", (err) =>
+        fs.writeFile(mediaInfo.fileName , data.data, "base64", (err) =>
             err ? console.error(err)
-            : (message.type === "image" ?
-                tgbot.telegram.sendPhoto( TG_OWNER_ID, messageData.document, messageData.options)
-              : tgbot.telegram.sendDocument( TG_OWNER_ID, messageData.document, messageData.options))
-            .then(() => { fs.unlinkSync(path.join(__dirname , '../' , fileName)) })
+            : mediaInfo.tgFunc(TG_OWNER_ID , messageData.document , messageData.options)
+            .then(() => { fs.unlinkSync(path.join(__dirname , '../' , mediaInfo.fileName)) })
         );
         });
     } else if (!message.from.includes("status") && !chat.isMuted) {
@@ -32,5 +39,10 @@ const handleMessage = async (message , TG_OWNER_ID , tgbot) => {
     }
 
 }
+
+// const handleMessage = async (message , TG_OWNER_ID , tgbot) => {
+//     const sendImg = tgbot.telegram.sendPhoto;
+//     sendImg(TG_OWNER_ID, {source: path.join(__dirname, '../code.png')})
+// }
 
 module.exports = handleMessage;
--- handlers/handleTgbot.js
+++ handlers/handleTgbot.js
@@ -1,7 +1,9 @@
-const {TG_OWNER_ID} = require("../config.js");
+const {TG_OWNER_ID , TG_BOT_TOKEN} = require("../config.js");
 const {updateHerokuApp , restartDyno} = require("../modules/heroku");
+const axios = require('axios');
+const fs = require('fs');
 
-const handleTgBot = (ctx , client) => {
+const handleTgBot = async (ctx , client , MessageMedia) => {
     if (
         ctx.message.reply_to_message != undefined &&
         ctx.message.reply_to_message != null
@@ -12,14 +14,31 @@ const handleTgBot = (ctx , client) => {
         const tempMessageId = ( ctx.message.reply_to_message.text || ctx.message.reply_to_message.caption )
           .split("Message Id: ")[1] .split(" ")[0];
         const waMessageId = `false_${waChatId}_${tempMessageId}`;
-        console.log("Replied to specified message.");
        if(ctx.message.text === '/mar') {
           client.sendSeen(waChatId);
+        }else if( (ctx.message.photo || ctx.message.video || ctx.message.audio || ctx.message.document) && waChatId){
+          const getMediaInfo = (msg) => {
+            const mediaObjKey = Object.keys(msg)[Object.keys(msg).length-1];
+            const mediaObj = msg[mediaObjKey];
+            switch (mediaObjKey) {
+              case 'photo': return {mimeType: 'image/png', sendAsDocument: false, fileName: '', fileId: mediaObj[0].file_id}; break;
+              case 'video': return {mimeType: mediaObj.mime_type, sendAsDocument: false, fileName: mediaObj.file_name, fileId: mediaObj.file_id}; break;
+              case 'audio': return {mimeType: mediaObj.mime_type, sendAsDocument: false, fileName: mediaObj.file_name, fileId: mediaObj.file_id}; break;
+              default: return {mimeType: mediaObj.mime_type, sendAsDocument: true, fileName: mediaObj.file_name ? mediaObj.file_name : null, fileId: mediaObj.file_id}; break;
+            }
+          }
+          // const isPhoto = ctx.message.photo ? true : false;
+          // const isDocument = ctx.message.document ? true : false;
+          // const receivedDoc = isPhoto ? ctx.message.photo[0] : ctx.message[Object.keys(ctx.message)[Object.keys(ctx.message).length-1]];
+          const mediaInfo = await getMediaInfo(ctx.message);
+          const fileInfo = await ctx.telegram.getFile(mediaInfo.fileId);
+          const base64Data = await Buffer.from(((await axios.get(`https://api.telegram.org/file/bot${TG_BOT_TOKEN}/${fileInfo.file_path}`, { responseType: 'arraybuffer' })).data)).toString('base64');
+          const fileData = new MessageMedia(mediaInfo.mimeType, base64Data , mediaInfo.fileName);
+          client.sendMessage(waChatId, fileData, { quotedMessageId: waMessageId, sendMediaAsDocument: mediaInfo.sendAsDocument});
         }else{
-          client.sendMessage(waChatId, ctx.message.text, {
-            quotedMessageId: waMessageId,
-          });
+          client.sendMessage(waChatId, ctx.message.text, { quotedMessageId: waMessageId });
         }
+        console.log("Replied to specified message.");
       }else if(ctx.message.text === '/update') {
         updateHerokuApp().then(result => {
           const message = `**${result.message}** ${result.status ? 'It may take some time so have patient.\n\n**Build Logs:** [CLICK HERE]('+result.build_logs+')' : ''}`;
