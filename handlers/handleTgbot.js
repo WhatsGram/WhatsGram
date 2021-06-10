dbfba0219fdfb1831532c5e7cd364fdc1b946abe
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-06-10 16:01:14 +0530

    Trying to fix sending and replying msgs through tg

--- handlers/handleTgbot.js
+++ handlers/handleTgbot.js
@@ -18,40 +18,44 @@ const getMediaInfo = (msg) => {
 
 const handleTgBot = async (ctx , client , MessageMedia) => {
   const sendMsgToWa = async (msg, chatId, msgId) => {
+    const waChat = chatId.includes('-') ? chatId + '@g.us' : chatId + '@c.us';
     if(!msg.text && chatId){
       const mediaInfo = await getMediaInfo(msg);
       const fileInfo = await ctx.telegram.getFile(mediaInfo.fileId);
       const base64Data = await Buffer.from(((await axios.get(`https://api.telegram.org/file/bot${TG_BOT_TOKEN}/${fileInfo.file_path}`, { responseType: 'arraybuffer' })).data)).toString('base64');
       const fileData = new MessageMedia(mediaInfo.mimeType, base64Data , mediaInfo.fileName);
-      client.sendMessage(chatId, fileData, { quotedMessageId: msgId ? msgId : null, sendMediaAsDocument: mediaInfo.SAD, sendAudioAsVoice:mediaInfo.SAV, caption:mediaInfo.caption });
+      client.sendMessage(waChat, fileData, { quotedMessageId: msgId ? msgId : null, sendMediaAsDocument: mediaInfo.SAD, sendAudioAsVoice:mediaInfo.SAV, caption:mediaInfo.caption });
     }else{
-      client.sendMessage(chatId, msg.text.startsWith('/send') ? msg.text.split('|')[2].trim() : msg.text.trim(), { quotedMessageId: msgId ? msgId : null });
+      const message = msg.text.startsWith('/send') ? msg.text.split(chatId)[1].trim() : msg.text;
+      console.log(message); 
+      client.sendMessage(waChat, message, { quotedMessageId: msgId ? msgId : null });
     }
+  }
+  const getIds = () => {
+    const tempChatId = ( ctx.message.reply_to_message.text || ctx.message.reply_to_message.caption )
+    .split("Chat Id: ")[1] .split(" ")[0];
+    const waChatId = tempChatId.includes("-") ? `${tempChatId}@g.us` : `${tempChatId}@c.us`;
+    const tempMessageId = ( ctx.message.reply_to_message.text || ctx.message.reply_to_message.caption )
+      .split("Msg Id: ")[1] .split(" ")[0];
+    const waMessageId = `false_${waChatId}_${tempMessageId}`;
+    return waChatId ? {waChatId, waMessageId} : null;
+  }
+  const tgResponse = msg => {
+    ctx.reply(msg, {reply_to_message_id:ctx.message.message_id, allow_sending_without_reply:true});
   }
   
   if(ctx.message.from.id == TG_OWNER_ID){
-    if (
-        ctx.message.reply_to_message != undefined &&
-        ctx.message.reply_to_message != null
-      ) {
-        const getIds = () => {
-          const tempChatId = ( ctx.message.reply_to_message.text || ctx.message.reply_to_message.caption )
-          .split("Chat Id: ")[1] .split(" ")[0];
-          const waChatId = tempChatId.includes("-") ? `${tempChatId}@g.us` : `${tempChatId}@c.us`;
-          const tempMessageId = ( ctx.message.reply_to_message.text || ctx.message.reply_to_message.caption )
-            .split("Msg Id: ")[1] .split(" ")[0];
-          const waMessageId = `false_${waChatId}_${tempMessageId}`;
-          return {waChatId, waMessageId};
-        } 
-       if(ctx.message.text === '/mar') {
+    if ( ctx.message.reply_to_message) {
+        if(ctx.message.text === '/mar' && getIds().waChatId) {
           client.sendSeen(getIds().waChatId);
-        }else if(ctx.message.text.startsWith('/send')){
-          const chatId = ctx.message.text.split('|')[1].trim() + '@c.us';
-          sendMsgToWa(ctx.message.reply_to_message, chatId)  
-        }else { sendMsgToWa(ctx.message, getIds().waChatId, getIds().waMessageId) }
-        console.log("Replied to specified message.");
+        }else if(ctx.message.text && ctx.message.text.startsWith('/send')){
+            const chatId = ctx.message.text.split(' ')[1].trim();
+            sendMsgToWa(ctx.message.reply_to_message, chatId).then(() => tgResponse('Message sent successfully.'));
+        }else if(getIds().waChatId){
+          sendMsgToWa(ctx.message, getIds().waChatId, getIds().waMessageId).then(() => tgResponse('Replied successfully.'));
+        }
       }else if(ctx.message.text.startsWith('/send')) {
-        const chatId = ctx.message.text.split('|')[1].trim() + '@c.us';
+        const chatId = ctx.message.text.split(' ')[1].trim();
         sendMsgToWa(ctx.message.reply_to_message ? ctx.message.reply_to_message : ctx.message, chatId); 
       }else if(ctx.message.text === '/update') {
         updateHerokuApp().then(result => {
