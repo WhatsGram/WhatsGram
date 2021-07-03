b65c866ab7cc2b536029476225f6b535e4d83456
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-07-03 10:44:27 +0530

    Add PMGUARD_ACTION, OCR_SAPCE_API_KEY

--- config.js
+++ config.js
@@ -9,5 +9,7 @@ const HEROKU_API_KEY = process.env.HEROKU_API_KEY || '';
 const HEROKU_APP_NAME = process.env.HEROKU_APP_NAME || '';
 const DB_URL = process.env.DB_URL || '';
 const pmguard_enabled = process.env.PMGUARD_ENABLED || 'false';
+const PMGUARD_ACTION = process.env.PMGUARD_ACTION && (process.env.PMGUARD_ACTION == 'mute' || process.env.PMGUARD_ACTION == 'block') ? process.env.PMGUARD_ACTION : 'mute';
+const OCR_SPACE_API_KEY = process.env.OCR_SPACE_API_KEY || '';
 
-module.exports = { TG_BOT_TOKEN , TG_OWNER_ID , REMOVE_BG_API, HEROKU_API_KEY, HEROKU_APP_NAME, DB_URL, pmguard_enabled}
+module.exports = { TG_BOT_TOKEN , TG_OWNER_ID , REMOVE_BG_API, HEROKU_API_KEY, HEROKU_APP_NAME, DB_URL, pmguard_enabled, PMGUARD_ACTION, OCR_SPACE_API_KEY}
