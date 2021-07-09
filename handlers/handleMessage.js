80756e0ae5d6b0500c5e9d255746a2c0d2927298
rashidpathiyil <rashidpathiyil@gmail.com>
2021-07-09 17:50:38 +0530

    Added ability to chat without ids on caption

--- handlers/handleMessage.js
+++ handlers/handleMessage.js
@@ -4,62 +4,62 @@ var path = require('path');
 const pmguard = require('../modules/pmguard');
 const config = require('../config')
 
-const handleMessage = async (message , TG_OWNER_ID , tgbot, client) => {
+const handleMessage = async (message, TG_OWNER_ID, tgbot, client) => {
     const getMediaInfo = (msg) => {
         switch (msg.type) {
-            case 'image': return {fileName: 'image.png' , tgFunc: tgbot.telegram.sendPhoto.bind(tgbot.telegram)}; break;
-            case 'video': return {fileName: 'video.mp4' , tgFunc: tgbot.telegram.sendVideo.bind(tgbot.telegram)}; break;
-            case 'audio': return {fileName: 'audio.m4a' , tgFunc: tgbot.telegram.sendAudio.bind(tgbot.telegram)}; break;
-            case 'ptt': return {fileName: 'voice.ogg' , tgFunc: tgbot.telegram.sendVoice.bind(tgbot.telegram)}; break;
-            default: return {fileName: msg.body , tgFunc: tgbot.telegram.sendDocument.bind(tgbot.telegram)}; break;
+            case 'image': return { fileName: 'image.png', tgFunc: tgbot.telegram.sendPhoto.bind(tgbot.telegram) }; break;
+            case 'video': return { fileName: 'video.mp4', tgFunc: tgbot.telegram.sendVideo.bind(tgbot.telegram) }; break;
+            case 'audio': return { fileName: 'audio.m4a', tgFunc: tgbot.telegram.sendAudio.bind(tgbot.telegram) }; break;
+            case 'ptt': return { fileName: 'voice.ogg', tgFunc: tgbot.telegram.sendVoice.bind(tgbot.telegram) }; break;
+            default: return { fileName: msg.body, tgFunc: tgbot.telegram.sendDocument.bind(tgbot.telegram) }; break;
         }
     }
-    
+
     const chat = await message.getChat();
-    const contact = await message.getContact(); 
+    const contact = await message.getContact();
     let name = contact.name || contact.pushname;
 
     if (message.author == undefined && config.pmguard_enabled == "true") { // Pm check for pmpermit module
         var checkPerm = await pmguard.handlePm(message.from.split("@")[0], name);
         if (checkPerm == "allowed") {
-            
+
         } else if (checkPerm.action == true && chat.isMuted == false) { // mute 
             message.reply(checkPerm.msg);
-            if(config.PMGUARD_ACTION == 'block') { await contact.block() }
-            else{
+            if (config.PMGUARD_ACTION == 'block') { await contact.block() }
+            else {
                 const d = new Date();
-                await chat.mute(new Date(d.getFullYear() +1, d.getMonth(), d.getDate()));
+                await chat.mute(new Date(d.getFullYear() + 1, d.getMonth(), d.getDate()));
             }
         } else if (chat.isMuted == true) {
-            
+
         } else if (checkPerm == "error") {
-            
+
         } else {
             message.reply(checkPerm.msg)
         }
 
     }
 
-    const tgMessage = `New message arrived on WhatsApp \n<b>From:</b> ${ chat.isGroup ? `${chat.name} | <a href="https://wa.me/${ message.author.split("@")[0] }">${name}</a>`
-        : `<a href="https://wa.me/'${message.from.split("@")[0]}">${chat.name}</a>`
-    }. \n<b>Chat Id: </b><code>${message.from.split("@")[0]}</code> \n<b>Msg Id: </b><code>${ message.id.id }</code> \n\n${message.body ? `<b>Message:</b> \n${message.body}` : ""}`;
+    const tgMessage = `From ${chat.isGroup ? `${chat.name} | <a href="https://wa.me/${message.author.split("@")[0]}?chat_id=${message.from.split("@")[0]}&message_id=${message.id.id}">${name}</a>`
+        : `<a href="https://wa.me/'${message.from.split("@")[0]}?chat_id=${message.from.split("@")[0]}&message_id=${message.id.id}">${chat.name}</a>`
+        }. \n${message.body ? `\n${message.body}` : ""}`;
 
     if (message.hasMedia && !chat.isMuted) {
         await message.downloadMedia().then(async (data) => {
-        const mediaInfo = await getMediaInfo(message);
-        const messageData = {
-            document : {source: path.join(__dirname , '../' , mediaInfo.fileName)},
-            options: { caption: tgMessage, disable_web_page_preview: true, parse_mode: "HTML" }
-        }
-        fs.writeFile(mediaInfo.fileName , data.data, "base64", (err) =>
-            err ? console.error(err)
-            : mediaInfo.tgFunc(TG_OWNER_ID , messageData.document , messageData.options)
-            .then(() => { fs.unlinkSync(path.join(__dirname , '../' , mediaInfo.fileName)) })
-        );
+            const mediaInfo = await getMediaInfo(message);
+            const messageData = {
+                document: { source: path.join(__dirname, '../', mediaInfo.fileName) },
+                options: { caption: tgMessage, disable_web_page_preview: true, parse_mode: "HTML" }
+            }
+            fs.writeFile(mediaInfo.fileName, data.data, "base64", (err) =>
+                err ? console.error(err)
+                    : mediaInfo.tgFunc(TG_OWNER_ID, messageData.document, messageData.options)
+                        .then(() => { fs.unlinkSync(path.join(__dirname, '../', mediaInfo.fileName)) })
+            );
         });
     } else if (!message.from.includes("status") && !chat.isMuted) {
-        tgbot.telegram.sendMessage(TG_OWNER_ID, tgMessage, 
-        { parse_mode: "HTML", disable_web_page_preview: true, disable_notification: chat.isMuted});
+        tgbot.telegram.sendMessage(TG_OWNER_ID, tgMessage,
+            { parse_mode: "HTML", disable_web_page_preview: true, disable_notification: chat.isMuted });
     }
 
 }
--- handlers/handleTgbot.js
+++ handlers/handleTgbot.js
@@ -1,74 +1,83 @@
-const {TG_OWNER_ID , TG_BOT_TOKEN} = require("../config.js");
-const {updateHerokuApp , restartDyno} = require("../modules/heroku");
+const { TG_OWNER_ID, TG_BOT_TOKEN } = require("../config.js");
+const { updateHerokuApp, restartDyno } = require("../modules/heroku");
 const axios = require('axios');
 const fs = require('fs');
 
 const getMediaInfo = (msg) => {
-  const mediaType = msg.photo ? 'photo' : msg.video ? 'video' : msg.audio ? 'audio' : msg.voice ? 'voice' : msg.sticker && !msg.sticker.is_animated ? 'sticker' : 'document'; 
+  const mediaType = msg.photo ? 'photo' : msg.video ? 'video' : msg.audio ? 'audio' : msg.voice ? 'voice' : msg.sticker && !msg.sticker.is_animated ? 'sticker' : 'document';
   const mediaObj = msg[mediaType];
-  const [type, mimeType, SAD, fileName, fileId, caption, SAV] = [mediaType,mediaObj.mime_type ? mediaObj.mime_type : '',false,null,mediaObj.file_id?mediaObj.file_id:mediaObj[0].file_id,msg.caption?msg.caption:'',mediaType=='voice'];
+  const [type, mimeType, SAD, fileName, fileId, caption, SAV] = [mediaType, mediaObj.mime_type ? mediaObj.mime_type : '', false, null, mediaObj.file_id ? mediaObj.file_id : mediaObj[0].file_id, msg.caption ? msg.caption : '', mediaType == 'voice'];
   switch (mediaType) {
-    case 'photo': return {type, mimeType: 'image/png', SAD, fileName, fileId, caption,SAV}; break;
-    case 'video': return {type, mimeType, SAD, fileName, fileId, caption, SAV}; break;
-    case 'audio': return {type, mimeType, SAD, fileName, fileId, caption, SAV}; break;
-    case 'voice': return {type, mimeType, SAD, fileName, fileId, caption, SAV}; break;
-    case 'sticker': return {type, mimeType: 'image/webp', SAD, fileName, fileId, caption, SAV, SAS:true }; break;
-    default: return {type, mimeType, SAD: true, fileName: mediaObj.file_name ? mediaObj.file_name : null, fileId, caption, SAV}; break;
+    case 'photo': return { type, mimeType: 'image/png', SAD, fileName, fileId, caption, SAV }; break;
+    case 'video': return { type, mimeType, SAD, fileName, fileId, caption, SAV }; break;
+    case 'audio': return { type, mimeType, SAD, fileName, fileId, caption, SAV }; break;
+    case 'voice': return { type, mimeType, SAD, fileName, fileId, caption, SAV }; break;
+    case 'sticker': return { type, mimeType: 'image/webp', SAD, fileName, fileId, caption, SAV, SAS: true }; break;
+    default: return { type, mimeType, SAD: true, fileName: mediaObj.file_name ? mediaObj.file_name : null, fileId, caption, SAV }; break;
   }
 }
 
-const handleTgBot = async (ctx , client , MessageMedia) => {
+const handleTgBot = async (ctx, client, MessageMedia) => {
   const sendMsgToWa = async (msg, chatId, msgId) => {
-    if(!msg.text && chatId){
+    if (!msg.text && chatId) {
       const mediaInfo = await getMediaInfo(msg);
       const fileInfo = await ctx.telegram.getFile(mediaInfo.fileId);
       const base64Data = await Buffer.from(((await axios.get(`https://api.telegram.org/file/bot${TG_BOT_TOKEN}/${fileInfo.file_path}`, { responseType: 'arraybuffer' })).data)).toString('base64');
-      const fileData = new MessageMedia(mediaInfo.mimeType, base64Data , mediaInfo.fileName);
-      client.sendMessage(chatId, fileData, { quotedMessageId: msgId ? msgId : null, sendMediaAsDocument: mediaInfo.SAD, sendAudioAsVoice:mediaInfo.SAV, caption:mediaInfo.caption ,sendMediaAsSticker:mediaInfo.SAS});
-    }else{
+      const fileData = new MessageMedia(mediaInfo.mimeType, base64Data, mediaInfo.fileName);
+      client.sendMessage(chatId, fileData, { quotedMessageId: msgId ? msgId : null, sendMediaAsDocument: mediaInfo.SAD, sendAudioAsVoice: mediaInfo.SAV, caption: mediaInfo.caption, sendMediaAsSticker: mediaInfo.SAS });
+    } else {
       const message = msg.text.startsWith('/send') ? msg.text.split(chatId.split('@')[0])[1].trim() : msg.text;
       client.sendMessage(chatId, message, { quotedMessageId: msgId ? msgId : null });
     }
   }
   const getIds = () => {
-    const tempChatId = ( ctx.message.reply_to_message.text || ctx.message.reply_to_message.caption )
-    .split("Chat Id: ")[1] .split(" ")[0];
-    const waChatId = tempChatId.includes("-") ? `${tempChatId}@g.us` : `${tempChatId}@c.us`;
-    const tempMessageId = ( ctx.message.reply_to_message.text || ctx.message.reply_to_message.caption )
-      .split("Msg Id: ")[1] .split(" ")[0];
-    const waMessageId = `false_${waChatId}_${tempMessageId}`;
-    return waChatId ? {waChatId, waMessageId} : null;
+    const url_string = () => {
+      if (ctx.message.reply_to_message.caption_entities) {
+        return ctx.message.reply_to_message.caption_entities[0].url
+      }
+      if (ctx.message.reply_to_message.entities) {
+        return ctx.message.reply_to_message.entities[0].url
+      }
+    }
+    const url = new URL(url_string());
+    const getChatId = url.searchParams.get("chat_id");
+    const getWaMessageId = url.searchParams.get("message_id");
+    const waChatId = getChatId.includes("-") ? `${getChatId}@g.us` : `${getChatId}@c.us`;
+    const waMessageId = `false_${waChatId}_${getWaMessageId}`;
+    return waChatId ? { waChatId, waMessageId } : null;
   }
   const tgResponse = msg => {
-    ctx.reply(msg, {reply_to_message_id:ctx.message.message_id, allow_sending_without_reply:true});
+    ctx.reply(msg, { reply_to_message_id: ctx.message.message_id, allow_sending_without_reply: true });
   }
-  
-  if(ctx.message.from.id == TG_OWNER_ID){
-    if ( ctx.message.reply_to_message) {
-        if(ctx.message.text === '/mar' && getIds().waChatId) {
-          client.sendSeen(getIds().waChatId);
-        }else if(ctx.message.text && ctx.message.text.startsWith('/send')){
-            const chatId = ctx.message.text.split(' ')[1].trim() + '@c.us';
-            sendMsgToWa(ctx.message.reply_to_message, chatId).then(() => tgResponse('Message sent successfully.'));
-        }else if(getIds().waChatId){
-          sendMsgToWa(ctx.message, getIds().waChatId, getIds().waMessageId).then(() => tgResponse('Replied successfully.'));
-        }
-      }else if(ctx.message.text.startsWith('/send')) {
+
+  if (ctx.message.from.id == TG_OWNER_ID) {
+    if (ctx.message.reply_to_message) {
+      if (ctx.message.text === '/mar' && getIds().waChatId) {
+        client.sendSeen(getIds().waChatId);
+      } else if (ctx.message.text && ctx.message.text.startsWith('/send')) {
         const chatId = ctx.message.text.split(' ')[1].trim() + '@c.us';
-        sendMsgToWa(ctx.message.reply_to_message ? ctx.message.reply_to_message : ctx.message, chatId); 
-      }else if(ctx.message.text === '/update') {
-        updateHerokuApp().then(result => {
-          const message = `**${result.message}** ${result.status ? 'It may take some time so have patient.\n\n**Build Logs:** [CLICK HERE]('+result.build_logs+')' : ''}`;
-          ctx.reply( message , { parse_mode: "markdown", disable_web_page_preview: true, reply_to_message_id:ctx.update.message.message_id, allow_sending_without_reply:true});
-      })}else if(ctx.message.text === '/restart') {
-        restartDyno().then(result => {
-          const message = `**${result.message}**`;
-          ctx.reply( message , { parse_mode: "markdown", disable_web_page_preview: true, reply_to_message_id:ctx.update.message.message_id, allow_sending_without_reply:true});
-      })}else {
-        ctx.reply("Reply to a message to send reply on WhatsApp");
+        sendMsgToWa(ctx.message.reply_to_message, chatId).then(() => tgResponse('Message sent successfully.'));
+      } else if (getIds().waChatId) {
+        sendMsgToWa(ctx.message, getIds().waChatId, getIds().waMessageId).then(() => tgResponse('Replied successfully.'));
       }
-  }else{
-    ctx.reply('You\'re not allowed to this')
+    } else if (ctx.message.text.startsWith('/send')) {
+      const chatId = ctx.message.text.split(' ')[1].trim() + '@c.us';
+      sendMsgToWa(ctx.message.reply_to_message ? ctx.message.reply_to_message : ctx.message, chatId);
+    } else if (ctx.message.text === '/update') {
+      updateHerokuApp().then(result => {
+        const message = `**${result.message}** ${result.status ? 'It may take some time so have patient.\n\n**Build Logs:** [CLICK HERE](' + result.build_logs + ')' : ''}`;
+        ctx.reply(message, { parse_mode: "markdown", disable_web_page_preview: true, reply_to_message_id: ctx.update.message.message_id, allow_sending_without_reply: true });
+      })
+    } else if (ctx.message.text === '/restart') {
+      restartDyno().then(result => {
+        const message = `**${result.message}**`;
+        ctx.reply(message, { parse_mode: "markdown", disable_web_page_preview: true, reply_to_message_id: ctx.update.message.message_id, allow_sending_without_reply: true });
+      })
+    } else {
+      ctx.reply("Reply to a message to send reply on WhatsApp");
+    }
+  } else {
+    // ctx.reply('You\'re not allowed to this')
   }
 }
 
