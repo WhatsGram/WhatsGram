5fb71d35aee4c11d4dcd35c32ad7b9e35046463c
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-09-15 22:34:03 +0530

    Add restart functionality
    Autostart WhatsApp client after every 12 hrs

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if (process.env.SESSION_DATA) {
 const cmd = (cmd, desc) => ({command: cmd, description: desc});
 tgbot.telegram.setMyCommands([cmd('start', 'Start bot.'), cmd('mar', 'Mark message as read.'), cmd('send', 'Ex: /send ph_no message'), cmd('update', 'Update UB.'), cmd('restart', 'Restart ub.')]);
 
-const client = new Client({
+const client = new Client({ // Create client.
   session: sessionData,
   puppeteer: { headless: true, args: ["--no-sandbox"] },
 });
@@ -51,10 +51,10 @@ async function generateQr() {
     await qrcode.generate(qr, { small: true });
     setTimeout(() => {
       console.log("Didn't found any response, exiting...");
-      process.exit();
+      return 
     }, 90 * 1000);
   });
-  client.on("authenticated", async (session) => {
+  client.on("authenticated", async (session) => { // Take action when user Authenticated successfully.
     if(config.HEROKU_APP_NAME && config.HEROKU_API_KEY){
       await setHerokuVar('SESSION_DATA' , JSON.stringify(session)).then(result => {
         if(result.message == errorMsg) 
@@ -65,13 +65,13 @@ async function generateQr() {
     await console.log( JSON.stringify(session) + "\n\nCopy above session and set it to heroku vars as SESSION_DATA" );
     await fs.writeFileSync("session.json", JSON.stringify(session));
   });
-  client.on("logout", () => {
+  client.on("logout", () => { // Take action when user logout.
     console.log( "Looks like you've been logged out. Please generate session again." );
     if (fs.existsSync("session.json")) fs.unlinkSync("session.json");
   });
 }
 
-if (!sessionData) {
+if (!sessionData) { // Check session data
   console.log("Session data not found. Generating QR please wait...");
   generateQr();
 } else {
@@ -79,7 +79,7 @@ if (!sessionData) {
   tgbot.telegram.sendMessage(config.TG_OWNER_ID, "Session data found. Logging in....", {disable_notification: true});
 }
 
-client.on("auth_failure" , reason => {
+client.on("auth_failure" , reason => { // If failed to log in.
   if(fs.existsSync('session.json')) fs.unlinkSync('session.json')
   const message = 'Failed to authenticate the client. Please fill env var again or generate session.json again. Generating session data again...';
   console.log(message);
@@ -88,7 +88,7 @@ client.on("auth_failure" , reason => {
   generateQr();
 })
 
-client.on("ready", () => {
+client.on("ready", () => { // Take actin when client is ready.
   const message = "Successfully logged in. Ready to rock!";
   console.log(message);
   tgbot.telegram.sendMessage( config.TG_OWNER_ID, message, {disable_notification: true});
@@ -102,23 +102,31 @@ tgbot.start(ctx => ctx.replyWithMarkdown(`Hey **${ctx.message.from.first_name}**
                       [{text:'Developer', url:'https://github.com/AffanTheBest'}, {text:'Donate', url:'https://ko-fi.com/affanthebest'}]]
   }}
 ));
-tgbot.command('donate', ctx => {
+tgbot.command('donate', ctx => { // Donate Command
   ctx.replyWithMarkdown('Thank you for showing intrest in donating! \nYou can donate me using following methods 👇\n\n*UPI Address*: `siddiquiaffan201@okaxis` \n\nOr you can use following links.',
   {disable_web_page_preview: true,
    reply_markup:{
      inline_keyboard: [[{text: 'Ko-fi', url: 'https://ko-fi.com/affanthebest'}, {text: 'Paypal', url: 'https://paypal.me/affanthebest'}]]
   }})
 });
+const restart = async (ctx) => {
+  if (ctx) await ctx.replyWithMarkdown('Restarting...', {disable_notification: true})
+  else tgbot.telegram.sendMessage(config.TG_OWNER_ID, 'Restarting...', {disable_notification: true})
+  await client.destroy();
+  await client.initialize();
+}
+tgbot.command('restart', ctx => restart(ctx)); // Restart WhatsApp Client using TG Bot.
+setInterval(() => restart(), 1000 * 60 * 60 * 12); // Auto restart WhatsApp client every 12 hours.
 
-tgbot.on("message", (ctx) => {
+tgbot.on("message", (ctx) => { // Listen TG Bot messages and take action
   handleTgBot(ctx , client , MessageMedia);
 });
 
-client.on("message", async (message) => {
+client.on("message", async (message) => { // Listen incoming WhatsApp messages and take action
   handleMessage(message , config.TG_OWNER_ID , tgbot, client);
 });
 
-client.on('message_create' , async (msg) => {
+client.on('message_create' , async (msg) => { // Listen outgoing WhatsApp messages and take action
   if (msg.body == "!alive") { // Alive command
     msg.delete(true)
     var aliveMsgData = await alive(await client.info.getBatteryStatus(), client.info.phone)
@@ -132,5 +140,5 @@ client.on("disconnect", (issue) => {
   console.log( "WhatsApp has been disconnected due to" + issue + ". Please restart your dyno or do npm start." );
 });
 
-tgbot.launch();
-client.initialize();
+tgbot.launch(); // Initialize Telegram Bot
+client.initialize(); // Initialize WhatsApp Client
