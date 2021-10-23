b481ccfd484e267aa4cb5e9c7aafbc3fb9182b94
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-10-23 13:02:26 +0530

    Added !add module

--- handlers/handleCreateMsg.js
+++ handlers/handleCreateMsg.js
@@ -171,6 +171,13 @@ const handleCreateMsg = async (msg , client , MessageMedia) => {
             }else{
                 client.sendMessage(msg.to, data.msg);
             }
+        }else if (msg.body.startsWith('!add')){
+            msg.delete(true);
+            const chat = await msg.getChat();
+            if(chat.isGroup){
+                const ids = msg.body.replace('!add ', '').split(' ').map(num => num.replace(/[^0-9]/g, '') + '@c.us');
+                ids.length > 0 ? chat.addParticipants(ids) : client.reply('Please pass valid phone numbers.');
+            }
         }
         else if(msg.body.startsWith('!help')) {
             msg.delete(true);
