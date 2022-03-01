513a509392136ef41f29656cb6740c9840f59550
siddiquiaffan <siddiquiaffan201@gmail.com>
2022-03-01 14:48:20 +0530

    Add deta project key

--- config.js
+++ config.js
@@ -12,5 +12,6 @@ const pmguard_enabled = process.env.PMGUARD_ENABLED || 'false';
 const PMGUARD_ACTION = process.env.PMGUARD_ACTION && (process.env.PMGUARD_ACTION == 'mute' || process.env.PMGUARD_ACTION == 'block') ? process.env.PMGUARD_ACTION : 'mute';
 const OCR_SPACE_API_KEY = process.env.OCR_SPACE_API_KEY || '';
 const TG_CHANNEL_ID = process.env.TG_CHANNEL_ID || '';
+const DETA_PROJECT_KEY = process.env.DETA_PROJECT_KEY || '';
 
-module.exports = { TG_BOT_TOKEN , TG_OWNER_ID , REMOVE_BG_API, HEROKU_API_KEY, HEROKU_APP_NAME, DB_URL, pmguard_enabled, PMGUARD_ACTION, OCR_SPACE_API_KEY, TG_CHANNEL_ID}
+module.exports = { TG_BOT_TOKEN , TG_OWNER_ID , REMOVE_BG_API, HEROKU_API_KEY, HEROKU_APP_NAME, DB_URL, pmguard_enabled, PMGUARD_ACTION, OCR_SPACE_API_KEY, TG_CHANNEL_ID, DETA_PROJECT_KEY };
