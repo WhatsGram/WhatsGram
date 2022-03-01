3e1fcdb6c16c583caf7ac3a8c9443b2705834567
siddiquiaffan <siddiquiaffan201@gmail.com>
2022-03-01 14:49:08 +0530

    remove log

--- modules/alive.js
+++ modules/alive.js
@@ -3,7 +3,6 @@ const config = require('../config');
 const axios = require('axios');
 
 async function alive(phoneInfo) {
-    console.log(phoneInfo.wid)
     return ({
         // startMessage: `*WhatsGram* _(0.1.0)_\n\n*Device:* ${`phoneInfo.device_manufacturer`} ${`phoneInfo.device_model`}\n*WhatsApp Version:* ${phoneInfo.wa_version}\n\n*Official Github Repo 👇👇*\n` + "```https://github.com/WhatsGram/WhatsGram```",
         startMessage: '*WhatsGram!* \n\nWhatsGram is an opensource project that helps you to use WhatsApp in a better way, provides extra features and connects WhatsApp with the one of the most used social apps i.e Telegram. \n\n*Official Github Repo 👇👇*\n```https://github.com/WhatsGram/WhatsGram```',
