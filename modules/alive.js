87d259eb53a1283d24780143150c082ba45c5ee8
siddiquiaffan <siddiquiaffan201@gmail.com>
2022-08-03 15:49:29 +0530

    Use local image

--- modules/alive.js
+++ modules/alive.js
@@ -1,13 +1,14 @@
   
 const config = require('../config');
 const axios = require('axios');
+const fs = require('fs');
 
 async function alive(phoneInfo) {
     return ({
         // startMessage: `*WhatsGram* _(0.1.0)_\n\n*Device:* ${`phoneInfo.device_manufacturer`} ${`phoneInfo.device_model`}\n*WhatsApp Version:* ${phoneInfo.wa_version}\n\n*Official Github Repo 👇👇*\n` + "```https://github.com/WhatsGram/WhatsGram```",
         startMessage: '*WhatsGram!* \n\nWhatsGram is an opensource project that helps you to use WhatsApp in a better way, provides extra features and connects WhatsApp with the one of the most used social apps i.e Telegram. \n\n*Official Github Repo 👇👇*\n```https://github.com/WhatsGram/WhatsGram```',
         mimetype: "image/jpeg",
-        data: Buffer.from(((await axios.get('https://telegra.ph/file/24f96b69dd941ffb7688b.jpg', { responseType: 'arraybuffer' })).data)).toString('base64'),
+        data: fs.readFileSync('./static/logo.png').toString('base64'),
         filename: "aliveMedia.jpg"
     })
 }
